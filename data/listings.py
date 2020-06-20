a75f222ef27464b4b8c7a920b57eb528917990ab
Osmo Salomaa <otsaloma@iki.fi>
2020-06-20 17:09:20 +0300

    Refactor

--- data/listings.py
+++ data/listings.py
@@ -5,6 +5,9 @@ import dataiter as di
 # Same as for documentation.
 di.PRINT_MAX_WIDTH = 64
 
+def parse_price(price):
+    return int(float(price.lstrip("$").replace(",", "")))
+
 data = (
     di.DataFrame.read_csv("orig/listings.csv")
     .select("id",
@@ -13,13 +16,12 @@ data = (
             "accommodates",
             "square_feet",
             "price")
-
     .rename(hood="neighbourhood_group_cleansed")
     .rename(guests="accommodates")
     .rename(sqft="square_feet")
+    .modify(price=lambda data: [parse_price(x) for x in data.price])
 )
 
-data.price = [int(float(x.lstrip("$").replace(",", ""))) for x in data.price]
 print(data.head())
 data.write_csv("listings.csv")
 data.write_json("listings.json")
