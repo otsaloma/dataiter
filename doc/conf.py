8ed122b24f4810b765a68d22e2d7e6f9f85cbe88
Osmo Salomaa <otsaloma@iki.fi>
2020-07-23 00:10:35 +0300

    Set navigation depth

--- doc/conf.py
+++ doc/conf.py
@@ -54,6 +54,10 @@ import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+html_theme_options = {
+    'navigation_depth': 3,
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
