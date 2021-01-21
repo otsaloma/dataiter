710af18450d7053d4b3dbebfab358190cbaf9c5f
Osmo Salomaa <otsaloma@iki.fi>
2021-01-21 23:02:11 +0200

    Simplify

--- prof.py
+++ prof.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 
-import atexit
 import cProfile
 import dataiter as di
-import os
 import pstats
-import tempfile
 
 from dataiter import test
 
@@ -15,13 +12,11 @@ def data_frame(fname, nrow=1000000):
     data = data.rbind(*([data] * n))
     return data.head(nrow)
 
-def prof(data):
-    data.group_by("make", "model").aggregate(n=di.nrow)
-
-handle, fname = tempfile.mkstemp()
-atexit.register(os.remove, fname)
 data = data_frame("vehicles.csv")
-cProfile.run("prof(data)", fname)
-stats = pstats.Stats(fname)
-stats.sort_stats(pstats.SortKey.CUMULATIVE)
-stats.print_stats(25)
+pr = cProfile.Profile()
+pr.enable()
+data.group_by("make", "model").aggregate(n=di.nrow)
+pr.disable()
+ps = pstats.Stats(pr)
+ps.sort_stats("cumtime")
+ps.print_stats(20, "/dataiter/")
