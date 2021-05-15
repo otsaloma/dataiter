615b8ee45d5aaa228476618ce05c417224a384e4
Osmo Salomaa <otsaloma@iki.fi>
2021-05-15 15:52:29 +0300

    Bump copyright years

--- doc/conf.py
+++ doc/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = 'dataiter'
-copyright = '2020 Osmo Salomaa'
+copyright = '2020–2021 Osmo Salomaa'
 author = 'Osmo Salomaa'
 
 # The full version, including alpha/beta/rc tags
