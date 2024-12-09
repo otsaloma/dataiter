c392ca657381ce8cbdac18716c0cb81440fc13bc
Osmo Salomaa <otsaloma@iki.fi>
2024-12-09 22:55:57 +0200

    Remove unnecessary file

--- data/neighbourhoods.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import dataiter as di
-
-# Rewrite to have one line per feature.
-data = di.read_geojson("orig/neighbourhoods.geojson")
-data.write("neighbourhoods.geojson")
